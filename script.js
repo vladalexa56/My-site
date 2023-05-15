4fda42e2c95d0d385a1a13b61a3bcfaa83a7fe1b
vladalexa56 <vladalexa56@users.noreply.github.com>
2023-05-15 16:21:06 +0300

    first commit

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ const aboutMe = document.querySelector("#about-me");
 const seeScroll = document.querySelector(".see--more-scroll");
 const contactMe = document.querySelector("#contact--me");
 const letsDoIt = document.querySelector(".button");
+const header = document.querySelector(".header");
+const nav = document.querySelector(".navbar");
+const navHeight = nav.getBoundingClientRect().height;
 
-console.log(imgOverlay);
 const hidden = function (e) {
   this.classList.remove("hidden");
 };
 
 const add = function (e) {
-  console.log("salut");
   this.classList.add("hidden");
 };
 
 imgOverlay.forEach((val) => val.addEventListener("mouseenter", hidden));
 imgOverlay.forEach((val) => val.addEventListener("mouseleave", add));
-console.log();
 
 seeScroll.addEventListener("click", function (e) {
   e.preventDefault();
@@ -39,3 +39,55 @@ letsDoIt.addEventListener("click", function (e) {
     behavior: "smooth",
   });
 });
+
+// const stickyNav = function (entries) {
+//   const [entry] = entries;
+//   if (!entry.isIntersecting) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+//   console.log(entry);
+// };
+
+// const headerObserver = new IntersectionObserver(stickyNav, {
+//   root: null,
+//   threshold: 0,
+//   rootMargin: `-${navHeight}px`,
+// });
+
+// headerObserver.observe(header);
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, oberver) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  oberver.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target;
+    const siblings = link.closest(".navbar").querySelectorAll(".nav-link");
+    const logo = link.closest(".navbar").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
